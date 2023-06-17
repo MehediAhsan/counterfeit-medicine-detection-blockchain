4857a1a01820cefc25b1aed11ca4fef8398493b6
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-06-17 16:05:08 +0600

    install tailwind and aos pakage

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Counterfeit Medicine Detection and Prevention </h1>
+            <p>Through Blockchain-Based</p>
+        </div>
+    );
+};
+
+export default Header;
