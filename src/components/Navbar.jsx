93a9aec62d4f92e49049fe0096d3db0918053461
sohanemon <m.sohanemon@gmail.com>
2023-07-06 14:57:50 +0600

    rem: redundant

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,13 @@ const Navbar = () => {
     return (
             <div className="flex flex-wrap place-items-center">
   <section className="relative mx-auto">
-
     <nav className="flex justify-between bg-gray-100 text-black w-screen">
       <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-
         <ul className="hidden md:flex px-4 mx-auto font-bold font-heading space-x-12">
           <li><Link className="hover:text-yellow-500" to='/'>Home</Link></li>
           <li><Link className="hover:text-yellow-500" to='/manufacturer'>Product</Link></li>
           <li><Link className="hover:text-yellow-500" to='/aboutus'>About Us</Link></li>
         </ul>
-
       </div>
     </nav>
   </section>
