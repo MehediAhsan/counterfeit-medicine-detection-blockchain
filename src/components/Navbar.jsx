7edcc9680688d913ebbe0bf7e962e462e172a936
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-07-26 20:05:56 +0600

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-            <div class="flex flex-wrap place-items-center">
-  <section class="relative mx-auto">
+            <div className="flex flex-wrap place-items-center">
+  <section className="relative mx-auto">
       
-    <nav class="flex justify-between bg-gray-100 text-black w-screen">
-      <div class="px-5 xl:px-12 py-6 flex w-full items-center">
+    <nav className="flex justify-between bg-gray-100 text-black w-screen">
+      <div className="px-5 xl:px-12 py-6 flex w-full items-center">
         
-        <ul class="hidden md:flex px-4 mx-auto font-bold font-heading space-x-12">
-          <li><Link class="hover:text-yellow-500" to='/'>Home</Link></li>
-          <li><Link class="hover:text-yellow-500" to='/manufacturer'>Manufacturer</Link></li>
-          <li><Link class="hover:text-yellow-500" to='/aboutus'>About Us</Link></li>
+        <ul className="hidden md:flex px-4 mx-auto font-bold font-heading space-x-12">
+          <li><Link className="hover:text-yellow-500" to='/'>Home</Link></li>
+          <li><Link className="hover:text-yellow-500" to='/manufacturer'>Manufacturer</Link></li>
+          <li><Link className="hover:text-yellow-500" to='/aboutus'>About Us</Link></li>
         </ul>
 
       </div>
